01d74cb3150098eab425b66bdf18ac6f1df00915
igor97st <x00128490@gmail.com>
2016-12-07 11:56:58 +0000

    lab 9 done

---
+++ playLab_CRUD-start/app/controllers/HomeController.java
@@ -0,0 +1,38 @@
+package controllers;
+
+import play.api.Environment;
+import play.mvc.*;
+import play.data.*;
+import play.db.ebean.Transactional;
+
+import java.util.*;
+import javax.inject.Inject;
+
+import views.html.*;
+
+//import models
+import models.*;
+
+
+
+public class HomeController extends Controller {
+
+    public Result index(String name) {
+        return ok(index.render("Welcome to the Home page", name));
+    }
+
+    public Result about() {
+        return ok(about.render());
+    }
+
+    public Result products() {
+        //Get the list of products
+        List<Product> productslist = Product.findAll();
+
+        // Render the list products 
+        return ok(products.render(productslist));
+    }
+
+}
+
+
